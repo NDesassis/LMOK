269ca19d4a46fc4c05021ca20985e5649af8074b
Xavier FREULON <xavier.freulon@minesparis.psl.eu>
2022-08-17 14:41:35 +0200

    bug fixing for M3

--- prepareDb.R
+++ prepareDb.R
@@ -1,4 +1,4 @@
-prepareDb = function(dat,nm_fac,nm_var,nm_sel,nm_type,type,sigma2=1)
+prepareDb = function(dat,nm_fac,nm_var,nm_sel,nm_type,type,sigma=1)
 {
   # re-initialisation de la base de données
   dat  <- db.locerase(dat, "z")
@@ -12,8 +12,9 @@ prepareDb = function(dat,nm_fac,nm_var,nm_sel,nm_type,type,sigma2=1)
   if(type%in%c("flat","house"))
   {
     tempsel = dat[,"C1NATURE"] == ifelse(type=="flat",2,1)
-    dat = db.add(dat,Un = 1.)
-    dat <- db.locate(dat, name = c("Un",nm_fac), loctype = "f")
+    dat = db.add(dat, Un = 1.)
+    dat <- db.locate(dat, name = c("Un", nm_fac), loctype = "f")
+    dat <- db.add(dat, sigma2 = sigma^2, loctype = "v")
   }else
   {
     tempsel = rep(1,dat$nech)
@@ -22,19 +23,17 @@ prepareDb = function(dat,nm_fac,nm_var,nm_sel,nm_type,type,sigma2=1)
     Un.b <- as.numeric(dat[,nm_type] == 2)
     N.a  <- dat[,nm_fac]; N.a[dat[,nm_type] != 1] <- 0.0 
     N.b  <- dat[,nm_fac]; N.b[dat[,nm_type] != 2] <- 0.0 
-    dat <- db.add(dat, Un.a, Un.b, N.a, N.b,loctype="f")
-    dat <- db.locate(dat, names = nm_type, loctype = "code", flag.locnew = TRUE)
+    dat  <- db.add(dat, Un.a, Un.b, N.a, N.b, loctype="f")
+    dat  <- db.locate(dat, names = nm_type, loctype = "code", flag.locnew = TRUE)
+    dat_sigma <- sigma[dat[, nm_type]]
+    dat  <- db.add(dat, sigma2 = dat_sigma^2, loctype = "v")
     
   }
-  
-  
+  # selection
   dat = db.add(dat, tempsel = tempsel)
   dat = db.rename(dat,"tempsel",nm_sel)
   dat = db.locate(dat,nm_sel,"sel")
-                  
+  # target variable     
   dat <- db.locate(dat, name = c(nm_var), loctype = "z", flag.locnew = TRUE)
-
- dat = db.add(dat,sigma2,loctype = "v")
-  #dat <- db.locate(dat, name = c("sel_train"), loctype = "sel", flag.locnew = TRUE)
   return(dat)
 }
