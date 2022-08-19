2b61fa284483322a74ae5d33f531f0b9f693bb1e
“NDesassis” <nicolas.desassis@gmail.com>
2022-08-19 17:22:42 +0200

    full

---
+++ LM.R
@@ -0,0 +1,7 @@
+fn_estim <- function(dbin, dbout, radix = "kfold_LM"){
+  lm(dbin[]$)
+  SPDE_LMOK_krigsim(dbin, dbout,
+                    model = m_est,
+                    mesh  = mesh,
+                    nsim  = 0, radix = radix, verbose = FALSE)
+}
