866e9a44ed6e268a8b8c75281e6c60262019eaf0
“NDesassis” <nicolas.desassis@gmail.com>
2022-10-18 10:48:17 +0200

    add lyon suburb filter

--- conf.R
+++ conf.R
@@ -21,15 +21,15 @@ nm_fac  <- "NB_cap"   # Name of the factor (number of rooms)
 nm_type <- "C1NATURE" # Name of the type (1 = House / 2 = Flat)
 
 # Definition of the meshing of the domain used for SPDE
-nodes   <- c(100,100) # Number of nodes in the main directions
+nodes   <- c(150,150) # Number of nodes in the main directions
 margin  <- 0.2        # Proportion to extend the bouding box of the domain
 param   <- list(margin = margin, nodes = nodes, nu = nu)
 
 # The mesh is computed by the script *makeMeshing.Rmd*
 coords = read.csv(file = nm_input_mesh)
 pts = db.create(x1=coords[,2],x2=coords[,3])
-mesh = meshing(pts,triswitch="nqQ")
-#mesh = NA
+#mesh = meshing(pts,triswitch="nqQ")
+mesh = NA
 #plot(mesh,lwd=0.1)
 #plot(poly,add=T)
 
@@ -45,11 +45,12 @@ N_adm = 500
 source(file = paste0("./param_", model,".R"))
 
 # Options for the optimizer (maximization of the likelihood)
-optim_maxit = 100
+optim_maxit = 200
 optim_method = "Nelder-Mead"
 
 
 #filters: a list which contains all the filters to apply. The filters are some functions
 #They can be written in the file "filter_examples.R" which already contains some examples.
 
-filter_list = list(good_cedantes)
+filter_list = list(lyon_suburb)
+#filter_list = list() # no filter
--- filter_examples.R
+++ filter_examples.R
@@ -2,3 +2,17 @@ good_cedantes = function(dat)
 {
   !dat[,"CED_CODE_ANON"]%in%c(5,9,16,14,22,23,28,35)
 }
+
+lyon_suburb = function(dat)
+{
+  #Centroid communes
+  centr = aggregate(dat[][c("X_TEMP","Y_TEMP")],by = list(dat[]$COM_INSEE),mean)
+  
+  #3ème arondissement de Lyon d'après :
+  #https://www.galichon.com/codesgeo/insee.php?commune=lyon&comm=1
+  lyon_insee  = 69383
+  coordslyon = as.numeric(centr[centr$Group.1==lyon_insee,c("X_TEMP","Y_TEMP")])
+  distlyon = sqrt((centr$X_TEMP-coordslyon[1])^2 + (centr$Y_TEMP-coordslyon[2])^2)
+  suburbs = centr$Group.1[distlyon < 10000]
+  dat[]$COM_INSEE%in%suburbs
+}
